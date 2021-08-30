4283658c0e967568d9d294bfba5895c7b60c9f3d
hanh kim <hanhkim134@gmail.com>
2021-08-30 18:54:08 +0700

    best solution

--- array/best-profit-stock.js
+++ array/best-profit-stock.js
@@ -5,35 +5,20 @@
  * @return {number}
  */
 const maxProfit = (prices) => {
-  let i = 0;
+  let i = 1;
   let n = prices.length;
-  let buyDate = -1;
   let profit = 0;
 
-  while (i < n - 1) {
-    // observe to sale
-    if (prices[i] >= prices[i + 1]) {
-      if (buyDate > -1) {
-        // bought => sale
-        profit += prices[i] - prices[buyDate];
-        buyDate = -1; // not have stock now
-      }
-    } else {
-      // observe to buy
-      if (buyDate === -1) {
-        buyDate = i;
-      }
+  while (i < n) {
+    if (prices[i] >= prices[i - 1]) {
+      // bought => sale
+      profit += prices[i] - prices[i - 1];
     }
     i++;
   }
 
-  // sale day is last in range
-  if (buyDate > -1) {
-    profit += prices[n - 1] - prices[buyDate];
-    buyDate = -1;
-  }
   return profit;
 };
 
-const prices = [7, 6, 4, 3, 1];
+const prices = [1, 2, 3, 4, 5];
 console.log(maxProfit(prices));
