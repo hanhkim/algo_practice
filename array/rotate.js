48ade08e59fb969aa835a89e74306f21a2ca7dc2
hanh kim <hanhkim134@gmail.com>
2021-08-31 14:59:58 +0700

    rotate 2

--- array/rotate.js
+++ array/rotate.js
@@ -6,12 +6,21 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 const rotate = (nums, k) => {
-  let goToSteps = k % nums.length;
-  const sliceKPos = nums.splice(nums.length - goToSteps, goToSteps);
-  nums.splice(0, 0, ...sliceKPos);
+  let n = nums.length;
+  if (k === n || n === 1 || k === 0) return;
+
+  k %= n;
+  let newNums = [...nums];
+  for (let i = 0; i < n; i++) {
+    if (i < k) newNums[i] = nums[n - k + i];
+    else newNums[i] = nums[i - k];
+  }
+  for (let i = 0; i < n; i++) {
+    nums[i] = newNums[i];
+  }
 };
 
-const arr = [1, 2];
+const arr = [1, 2, 3, 4, 5, 6, 7];
 const k = 5;
 
 rotate(arr, k);
